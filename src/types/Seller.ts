83efb5307b3c2e99592bcb4dace0ddbb33ab4dfa
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-07-07 21:55:06 +0200

    add useFetch composable and Seller type definitions

---
+++ src/types/Seller.ts
@@ -0,0 +1,23 @@
+type Team = 'POB' | 'SOL' | 'AP';
+
+interface SellerCategoryStats {
+  category: string;
+  unitsSold: number;
+  totalRevenue: number;
+}
+
+interface SellerMonthlySales {
+  month: string;
+  categories: SellerCategoryStats[];
+}
+
+export interface Seller {
+  sellerId: number;
+  name: string;
+  hireDate: string;
+  team: Team;
+  totalUnitsSold: number;
+  totalRevenue: number;
+  averageUnitsSoldPerMonth: number;
+  sales: SellerMonthlySales[];
+}
